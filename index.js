73da348b76fb41ed8c9465c32fa47c67151e57a1
Dan Flettre <fletd01@yahoo.com>
2015-08-03 18:22:10 -0500

    use -loglevel silent to avoid npm-debug.log

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function canHazPackage (name) {
   var valid = validateName(name).validForNewPackages
   if (!valid) return 'Package name does not conform to naming rules.'
 
-  var view = exec('npm view ' + name, {silent: true})
+  var view = exec('npm -loglevel silent view ' + name, {silent: true})
 
   if (view.code !== 1) return 'Sorry, that package name is taken! :('
 
